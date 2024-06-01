53f17d1807ab57d4aaf97efa6877b2712d211aa0
Sgechuki <sgechuki@outlook.com>
2024-06-01 07:19:27 +0000

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Task 5: Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    returns a salted, hashed password
+    which is a byte string
+    """
+    b = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(b, salt)
