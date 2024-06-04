5c6134fad74d8d301d1adb77ffb4e520a4295160
Sgechuki <sgechuki@outlook.com>
2024-06-04 10:17:53 +0000

    Raise 403

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> None:
     raise a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route("/forbidden", methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    """
+    raise a 403 error
+    """
+    abort(403)
