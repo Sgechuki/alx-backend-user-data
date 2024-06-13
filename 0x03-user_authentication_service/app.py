e81c9ebad699478b617dcefd9847ae9c07f8e706
Sgechuki <sgechuki@outlook.com>
2024-06-13 22:52:32 +0000

    Correct typo cookie to cookies

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,7 +59,7 @@ def logout():
     """
     destroy the session and redirect the user to GET /
     """
-    session_id = request.cookie.get("session_id")
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
